2b9359fe983483bbb932554f531527f16dc325a8
Vladimir Buyanov <kafatron@gmail.com>
2018-05-07 02:32:54 +0200

    Add dependency lock file and add small fix

--- main.go
+++ main.go
@@ -35,6 +35,7 @@ func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU() * goThreadsPerCPU)
 	objChan := make(chan Object, cli.Workers*4)
 	wg := sync.WaitGroup{}
+	prgBarQuit := make(chan bool)
 
 	for i := cli.Workers; i != 0; i-- {
 		wg.Add(1)
@@ -56,17 +57,9 @@ func main() {
 
 	log.Info("Starting sync\n")
 	counter.startTime = time.Now()
+
 	if isatty.IsTerminal(os.Stdout.Fd()) {
-		writer := uilive.New()
-		writer.Start()
-		go func() {
-			for {
-				dur := time.Since(counter.startTime).Seconds()
-				fmt.Fprintf(writer, "Synced: %d; Skipped: %d; Failed: %d; Total processed: %d\nAvg syncing speed: %.f obj/sec; Avg listing speed: %.f obj/sec\n",
-					counter.sucObjCnt, counter.skipObjCnt, counter.failObjCnt, counter.totalObjCnt, float64(counter.sucObjCnt)/dur, float64(counter.totalObjCnt)/dur)
-				time.Sleep(time.Second)
-			}
-		}()
+		go StartProgressBar(prgBarQuit)
 	}
 
 	if err := syncGr.Source.List(objChan); err != nil {
@@ -74,10 +67,13 @@ func main() {
 	}
 
 	wg.Wait()
+	if isatty.IsTerminal(os.Stdout.Fd()) {
+		prgBarQuit <- true
+	}
 	dur := time.Since(counter.startTime).Seconds()
 	log.Info("Sync finished successfully")
 	log.Infof("Synced: %d; Skipped: %d; Failed: %d; Total processed: %d", counter.sucObjCnt, counter.skipObjCnt, counter.failObjCnt, counter.totalObjCnt)
-	log.Infof("Avg syncing speed: %9.f obj/sec; Avg listing speed: %9.f obj/sec; Duration: %d sec\n", float64(counter.sucObjCnt)/dur, float64(counter.totalObjCnt)/dur, dur)
+	log.Infof("Avg syncing speed: %9.f obj/sec; Avg listing speed: %9.f obj/sec; Duration: %9.f sec\n", float64(counter.sucObjCnt)/dur, float64(counter.totalObjCnt)/dur, dur)
 }
 
 func ConfigureLogging() {
@@ -88,4 +84,20 @@ func ConfigureLogging() {
 	}
 	log.Formatter = &logrus.TextFormatter{}
 	log.Out = os.Stdout
+}
+
+func StartProgressBar(quit <-chan bool) {
+	writer := uilive.New()
+	writer.Start()
+	for {
+		select {
+		case <- quit:
+			return
+		default:
+			dur := time.Since(counter.startTime).Seconds()
+			fmt.Fprintf(writer, "Synced: %d; Skipped: %d; Failed: %d; Total processed: %d\nAvg syncing speed: %.f obj/sec; Avg listing speed: %.f obj/sec\n",
+				counter.sucObjCnt, counter.skipObjCnt, counter.failObjCnt, counter.totalObjCnt, float64(counter.sucObjCnt)/dur, float64(counter.totalObjCnt)/dur)
+			time.Sleep(time.Second)
+		}
+	}
 }
